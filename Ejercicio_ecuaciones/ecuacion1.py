e7bc47f6c53b2535cbbfff4b24650bb8b5235d45
lauralardies <lrodrlar@myuax.com>
2022-10-11 12:31:04 +0200

    ec 1, no sabemos qué significa el resultado

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -1,14 +1,12 @@
-import symbol
-from sympy import *
+import sympy
 
-def resolver():
-    x= symbols('x')
-    y= Function('y')
-    
-    edo= Eq(diff(y(x)),(x**2*y-y)/(y + 1))
-    CI={y(3):1}
-    dsolve(edo,y(x),ics=CI)
+class Ec_Dif_1():
+    def __init__(self) -> None:
+        self.x = sympy.Symbol('x')
+        self.f = sympy.Function('y')(self.x)
 
-
-
-
+    def resolver(self):
+        eq = ((self.x**2)*(self.f) - self.f) / (self.f + 1)
+        dif_eq = sympy.Eq(self.f.diff(self.x), eq)
+        sol = sympy.dsolve(dif_eq)
+        return sol
