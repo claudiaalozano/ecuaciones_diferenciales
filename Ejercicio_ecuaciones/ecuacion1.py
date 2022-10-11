1fe8320634caa1f6638be5c063b8352db789aa79
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-11 11:51:08 +0200

    probando, ns como solucionar error

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -5,7 +5,7 @@ def resolver():
     x= symbols('x')
     y= Function('y')
     
-    edo= Eq(diff(y(x)),(x**2*y-y)/(y + 1))
+    edo= Eq(diff(y(x)),((x**2*y)-y)/(y + 1))
     CI={y(3):1}
     dsolve(edo,y(x),ics=CI)
 
