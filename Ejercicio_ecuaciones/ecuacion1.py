b57d005353c7d420df147b15795cc307a65a1b90
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-11 11:16:50 +0200

    definimos la solucion

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -1,7 +1,12 @@
 import numpy 
 import matplotlib.pyplot as plt
 from scipy import integrate
+from sympy import O
 
 def f(y,x):
     return (x**2*y-y)/(y+1)
-    
+
+y0= 1.0
+x= numpy.linspace(0,1,100) # esto sirve para crear un vector de 100 puntos entre 0 y 1
+
+sol= integrate.odeint(f,y0,x) # esto es para resolver la ecuacion diferencial
