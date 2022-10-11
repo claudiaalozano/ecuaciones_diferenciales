eb957385f939ae6b8f5db8211926ed712d9cb7fd
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-11 12:08:54 +0200

    sale con otras euaciones

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -1,18 +1,22 @@
 import sympy
 sympy.init_printing(use_latex='mathjax') # Para que se vea bonito
 
+# ecuacion diferencial
+# y' = 2xy
+
 
 def resolver():
     x= sympy.Symbol('x')
     y= sympy.Function('y')
     
-    edo= sympy.Eq(y(x).diff(x),((x**2*y)-y)/(y + 1))
+    edo= sympy.Eq(y(x).diff(x), 2*x*y(x))
     CI={y(3):1}
     solucion= sympy.dsolve(edo, y(x), ics=CI)
     print(solucion)
     sympy.plot(solucion.rhs, (x, 0, 10), title='Solución de la EDO')
     
- 
+if __name__ == '__main__':
+    resolver()
 
 
 
