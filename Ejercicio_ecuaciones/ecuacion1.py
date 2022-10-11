5e2a4881c8c56290cb2fd324d443e3c48ca31d4e
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-11 11:56:21 +0200

    cambios

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -1,13 +1,18 @@
-import symbol
-from sympy import *
+import sympy
+sympy.init_printing(use_latex='mathjax') # Para que se vea bonito
+
 
 def resolver():
-    x= symbols('x')
-    y= Function('y')
+    x= sympy.Symbol('x')
+    y= sympy.Function('y')
     
-    edo= Eq(diff(y(x)),((x**2*y)-y)/(y + 1))
+    edo= sympy.Eq(y(x).diff(x),((x**2*y)-y)/(y + 1))
     CI={y(3):1}
-    dsolve(edo,y(x),ics=CI)
+    solucion= sympy.dsolve(edo, y(x), ics=CI)
+    print(solucion)
+    sympy.plot(solucion.rhs, (x, 0, 10), title='Solución de la EDO')
+    
+ 
 
 
 
