093a0f80ccac67834879e94ab29c2333a704d21a
claudiaalozano <clau.lozano.perez@gmail.com>
2022-10-13 12:46:16 +0200

    cambiiamos la visualización del resultado

--- Ejercicio_ecuaciones/ecuacion2.py
+++ Ejercicio_ecuaciones/ecuacion2.py
@@ -6,8 +6,9 @@ x = sym.symbols("x", real = True)
 eq = sym.Eq(y(x).diff(x)*sym.sin(x), y(x) * sym.log(y(x)))
 a = sym.dsolve(eq, ics ={y(sym.pi/2): sym.exp(1), sym.diff(y(x), x).subs(x, sym.pi/2): sym.exp(1)})
 print("La solución a la ecuación y`sen(x) = y ln(y) es:")
-print(a)
-sym.print_rcode(a)
+sym.pprint(a)
+
+
 #def lin():
    # print("\n")
 
