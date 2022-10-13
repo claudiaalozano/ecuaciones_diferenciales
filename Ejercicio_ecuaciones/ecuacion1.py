bb77128754feeb1ade510a730b4ee4410007b847
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-13 18:41:14 +0200

    cambios

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -2,3 +2,11 @@ import sympy as sym
 sym.init_printing(use_latex= True) # Para imprimir en formato LaTeX en la consola 
 y= sym.symbols("f", cls=sym.Function) # Definimos la función f  
 x= sym.symbols("x", real = True) # Definimos la variable x
+
+#Ecuacion diferencial: y'= x^2*y-y/1+y
+eq = sym.Eq(y(x).diff(x), x**2*y(x)-y(x)/(1+y(x))) # Definimos la ecuación diferencial
+a = sym.dsolve(eq, ics ={y(3): -1, sym.diff(y(x), x).subs(x, 0): 0}) # Resolvemos la ecuación diferencial
+print("La solución a la ecuación y'= x^2*y-y/(1+y) es:")
+sym.pprint(a) # Imprimimos la solución
+
+
