3583d58a3f2d66addb8777541a52278e67b20eb7
lauralardies <lrodrlar@myuax.com>
2022-10-13 23:38:31 +0200

    c

--- Ejercicio_ecuaciones/lanzador.py
+++ Ejercicio_ecuaciones/lanzador.py
@@ -3,14 +3,8 @@ import ecuacion2
 import ecuacion3
 import ecuacion4
 
-
-
 def iniciar():
     ecuacion1.resolver_1()
     ecuacion2.resolver_2()
     ecuacion3.resolver_3()
     ecuacion4.resolver_4()
-
-    
-
-
