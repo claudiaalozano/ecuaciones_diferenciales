7a6cfec55c527ccaf61cb9f148b6b1de3d2ded30
claudiaalozano <clau.lozano.perez@gmail.com>
2022-10-13 12:21:50 +0200

    defino la variable x e y

--- Ejercicio_ecuaciones/ecuacion2.py
+++ Ejercicio_ecuaciones/ecuacion2.py
@@ -1,18 +1,49 @@
-from sympy import *
-from sympy.interactive import printing
+import sympy as sym
+sym.init_printing(use_latex= True)
+y = sym.symbols("f", cls=sym.Function)
+x = sym.symbols("x")
 
-printing.init_printing(use_latex= True)
 
-def lin():
-    print("\n")
+#def lin():
+   # print("\n")
 
-    x = symbols("x")
-    y = symbols("y")
-    z = symbols("z")
+ #   x = symbols("x")
+ #   y = symbols("y")
+  #  z = symbols("z")
     
 
-    display(x)
-    display(y)
+   # print(x)
+   # print(y)
+  #  print(z)
 
-    f = Function("f")("x")
-    g = Function("g")("y")
+#    f = Function("f")("x")
+ #   g = Function("g")("y")
+#    M = Function("M")("z")
+
+
+ #   print(f)
+ #   print(g)
+ #   print(M)
+
+  #  h=Function("h")
+ #   h=h(x,y)
+ #   print(h)
+
+    #primera derivada
+   # df = diff(f,x)
+ #   dg = diff(g,y)
+  #  dhx = diff(h,x)
+ #   dhy = diff(h,y)
+
+ #   print(df)
+  #  lin()
+
+   # print(dg)
+   # lin()
+
+   # print(dhx)
+  #  lin()
+
+  #  print(dhy)
+  #  lin()
+#lin() 
