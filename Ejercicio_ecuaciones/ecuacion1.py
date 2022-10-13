ff73f080409cce65e58cf66405cccfcdd55df35c
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-13 18:30:50 +0200

    definimos la variable x

--- Ejercicio_ecuaciones/ecuacion1.py
+++ Ejercicio_ecuaciones/ecuacion1.py
@@ -1,3 +1,4 @@
 import sympy as sym
 sym.init_printing(use_latex= True) # Para imprimir en formato LaTeX en la consola 
 y= sym.symbols("f", cls=sym.Function) # Definimos la función f  
+x= sym.symbols("x", real = True) # Definimos la variable x
